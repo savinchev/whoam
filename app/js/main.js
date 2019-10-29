65fbc94a27407f1df3411d7d94d1eddbeb588ede
Le8wa-dev <savinchev@list.ru>
2019-10-29 18:33:43 +0300

    added section school & studentresults

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,9 @@
 $(function(){
 
-
+    $('.studentresults__slider').slick({
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/arrowleft.png" alt=""></button> ',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/arrowright.png" alt=""></button> ',
+    });
 
     
 });
