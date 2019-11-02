0f08a58195743c2797f827b74281d18592b8d235
Le8wa-dev <savinchev@list.ru>
2019-11-02 15:19:06 +0300

    added courseabout - contacts

--- app/js/main.js
+++ app/js/main.js
@@ -5,5 +5,13 @@ $(function(){
         nextArrow: '<button class="slick-arrow slick-next"><img src="images/arrowright.png" alt=""></button> ',
     });
 
+    $('.businesscard__diploms').slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        variableWidth: true,
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/arrowleft.png" alt=""></button> ',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/arrowright.png" alt=""></button> ',
+    });
+
     
 });
